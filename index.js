b48691a4bbc993e6de55d7d6a66a0bf5fd0a92fa
Brian <mahuibrian14@gmail.com>
2023-03-22 11:27:08 +0300

    1st commit

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code in this file!
+const currentUser = 'Rex';
+const welcomeMessage = "Welcome to Flatbook ," + currentUser + '!';
+//console.log(welcomeMessage);
+const excitedWelcomeMessage = 'WELCOME TO FLATBOOK,' +currentUser.toUpperCase() + '!';
+//console.log(excitedWelcomeMessage);
+const shortGreeting = 'Welcome,'+ currentUser[0] + '!';
+//console.log(shortGreeting);
